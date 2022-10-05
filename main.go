af32fd05d4f26d825c72c49a46afff6295cc05ad
f010 <mail.fawazabdulla@gmail.com>
2022-10-05 19:58:30 +0530

    add basic commands

--- main.go
+++ main.go
@@ -1,2 +1,46 @@
-func main () {
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	versionCmd := cobra.Command{
+		Use: "version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("cheeder version: %v\n", VERSION)
+		},
+	}
+
+	initCmd := cobra.Command{
+		Use: "seeder:create",
+		Run: Create,
+	}
+	cmds := &cobra.Command{Use: "cheeder"}
+	cmds.AddCommand(&versionCmd, &initCmd)
+	cmds.Execute()
 }
+
+func Create(c *cobra.Command, args []string) {
+	var dir string
+	switch len(args) {
+	case 0:
+		dir = "."
+	case 1:
+		dir = args[0]
+	default:
+		{
+			c.Help()
+			os.Exit(1)
+		}
+	}
+	fmt.Printf("dir: %v\n", dir)
+	// TODO: write seeder file
+}
+
+func Seed(c *cobra.Command, args []string) {}
+
+func Unseed(c *cobra.Command, args []string) {}
---
+++ static.go
@@ -0,0 +1,14 @@
+package main
+
+const VERSION string = "30.0"
+
+var Conf = `[database]
+# host is required (network host)
+# host =
+# port = 3306
+# database is required
+# database =
+# user defaults to OS user
+# user =
+# password =
+`
